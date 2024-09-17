d3174f057bd8c4bf0690904342145f5dc8878524
mdjespc <mdjespc@gmail.com>
2024-09-16 20:06:09 -0400

    Added gravity effects on player position

--- main.py
+++ main.py
@@ -31,14 +31,32 @@ class Player:
   Also handles in-game conditions & events that interact
   with the avatar.
   '''
+  is_jumping: bool
+  is_grounded: bool
+
   def __init__(self, image, pos):
     self.image = image
     self.pos = pos
     self.avatar = Avatar(self.image, self.pos)
+
     self.vel = Vector2(0, 0)
+    self.jump_height = 32
+    self.is_jumping = False
+    self.is_grounded = False
     
+  def jump(self):
+    self.vel.y = -self.jump_height
 
   def update(self):
+    if self.is_jumping and self.is_grounded:
+      self.jump()
+
+    if not self.is_grounded:
+      self.vel.y = min(self.vel.y + GRAVITY.y, 100) #Hardcapping falling speed at 100.
+
+    #Update the position the avatar should have
+    self.pos = (self.pos[0], self.pos[1] + self.vel.y)
+
     self.avatar.update(self.pos)
 
   def draw(self, surf):
@@ -56,12 +74,20 @@ while True:
       pygame.quit()
       sys.exit()
 
+  #Key press listener
+  keys = pygame.key.get_pressed()
+
+  if keys[pygame.K_SPACE]:
+    TEST_PLAYER.is_jumping = True
+
   surf.fill((244, 250, 252))
   surf.blit(BACKGROUND_IMAGE, (0, 0))
   for element in TEST_LEVEL.level_elements:
     element.update()
 
   TEST_PLAYER.update()
+ 
+
   TEST_PLAYER.draw(surf)
 
   for element in TEST_LEVEL.level_elements:
